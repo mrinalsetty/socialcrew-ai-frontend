108bdf8a87a0488a7d9b57f0f61586a27de44638
Mrinal Setty <mrinalsetty.s@gmail.com>
2026-02-28 19:46:56 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white dark:bg-black">
+      <h1 className="text-4xl font-semibold tracking-tight text-black dark:text-white">
+        Welcome to SocialCrew AI
+      </h1>
+    </div>
+  );
+}
